5a62094d9605b0a66a8329c0d3aca1be57b063f1
Mahmoud Zeus <meropvo17@gmail.com>
2024-09-29 09:04:59 +0300

    finish the style of Products page

--- app/products/[category]/page.js
+++ app/products/[category]/page.js
@@ -1,9 +1,11 @@
 import ProductCard from "@/components/products/ProductCard";
+import Line from "@/components/ui/line";
 
 export default function page() {
   return (
-    <div className="min-h-[50vh]">
-      <ProductCard />
+    <div className=" bg-[#f5f5f5] pt-10">
+      <Line Title="Contact Us" />
+      <div className="min-h-[50vh] grid lg:grid-cols-2 gap-10 p-10 lg:container mx-auto grid-cols-1 bg-[#f5f5f5]"></div>
     </div>
   );
 }
