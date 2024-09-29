750c6be365d6378290506f7dc1d2f398bd124eb5
Mahmoud Zeus <meropvo17@gmail.com>
2024-09-29 09:00:01 +0300

    finishing product card component

--- components/products/ProductCard.jsx
+++ components/products/ProductCard.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import img from "./imgs/logo-card.png";
-export default function ProductCard() {
+export default function ProductCard({ product, category }) {
   return (
-    <div className="shadow-md px-4 py-10 flex justify-between items-center p-5 rounded-md ">
-      <Image src={img} alt="logo" width={75} />
-      <p className="text-center text-black text-2xl mt-2">Product</p>
-      <p className="text-center text-gray-500 text-2xl mt-2">category</p>
+    <div className="shadow-md px-4  flex justify-between items-center p-5 rounded-xl bg-white ">
+      <Image src={img} alt="logo" width={75} className="hidden sm:block" />
+      <p className="text-center text-black text-xl mt-2">{product}</p>
+      <p className="text-center text-gray-500 text-lg mt-2">{category}</p>
     </div>
   );
 }
