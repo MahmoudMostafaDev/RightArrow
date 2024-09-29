c4627acd5d3d93a2dc1abe29b901cdf55edce1c1
Mahmoud Zeus <meropvo17@gmail.com>
2024-09-29 08:50:58 +0300

    first version of product card

--- components/products/ProductCard.jsx
+++ components/products/ProductCard.jsx
@@ -1,3 +1,11 @@
+import Image from "next/image";
+import img from "./imgs/logo-card.png";
 export default function ProductCard() {
-  return <div>ProductCard</div>;
+  return (
+    <div className="shadow-md px-4 py-10 flex justify-between items-center p-5 rounded-md ">
+      <Image src={img} alt="logo" width={75} />
+      <p className="text-center text-black text-2xl mt-2">Product</p>
+      <p className="text-center text-gray-500 text-2xl mt-2">category</p>
+    </div>
+  );
 }
